b00f6b4c627e8e79d7453a5cbd440b0a47515c5c
Drew <43252026+AOSTAPOVYCH@users.noreply.github.com>
2020-12-03 17:18:54 -0500

    stuff

--- Scripts/Vehicles/Vehicle.cs
+++ Scripts/Vehicles/Vehicle.cs
@@ -2,119 +2,114 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Sands
+public class Vehicle
 {
+    private string name;
+    private int health;
+    private int armour;
+    private double speed;
+    private int partySize;
+    private int damage;
+    private double weight;
 
-    public class Vehicle
+    public string Name
     {
-        private string name;
-        private int health;
-        private int armour;
-        private double speed;
-        private int buttSpace;
-        private int damage;
-        private double weight;
-
-        public string Name
+        get
         {
-            get
-            {
-                return name;
-            }
-
-            set
-            {
-                name = value;
-            }
-        }
-        public int Health
-        {
-            get
-            {
-                return health;
-            }
-
-            set
-            {
-                health = value;
-            }
+            return name;
         }
 
-        public int Armour
+        set
         {
-            get
-            {
-                return armour;
-            }
-
-            set
-            {
-                armour = value;
-            }
+            name = value;
         }
-
-        public double Speed
-        {
-            get
-            {
-                return speed;
-            }
-
-            set
-            {
-                speed = value;
-            }
-        }
-
-        public int ButtSpace
-        {
-            get
-            {
-                return buttSpace;
-            }
-
-            set
-            {
-                buttSpace = value;
-            }
-        }
-
-        public int Damage
-        {
-            get
-            {
-                return damage;
-            }
-
-            set
-            {
-                damage = value;
-            }
-        }
-
-        public double Weight
-        {
-            get
-            {
-                return weight;
-            }
-
-            set
-            {
-                weight = value;
-            }
-        }
-
-        public Vehicle(string name, int health, int armour, double speed, int buttspace, int damage, double weight)
-        {
-            this.name = name;
-            this.health = health;
-            this.armour = armour;
-            this.speed = speed;
-            this.buttSpace = buttSpace;
-            this.damage = damage;
-            this.weight = weight;
-        }
-
     }
+    public int Health
+    {
+        get
+        {
+            return health;
+        }
+
+        set
+        {
+            health = value;
+        }
+    }
+
+    public int Armour
+    {
+        get
+        {
+            return armour;
+        }
+
+        set
+        {
+            armour = value;
+        }
+    }
+
+    public double Speed
+    {
+        get
+        {
+            return speed;
+        }
+
+        set
+        {
+            speed = value;
+        }
+    }
+
+    public int PartySize {
+        get
+        {
+            return partySize;
+        }
+
+        set
+        {
+            partySize = value;
+        }
+    }
+
+    public int Damage
+    {
+        get
+        {
+            return damage;
+        }
+
+        set
+        {
+            damage = value;
+        }
+    }
+
+    public double Weight
+    {
+        get
+        {
+            return weight;
+        }
+
+        set
+        {
+            weight = value;
+        }
+    }
+
+    public Vehicle(string name, int health, int armour, double speed, int buttspace, int damage, double weight)
+    {
+        this.name = name;
+        this.health = health;
+        this.armour = armour;
+        this.speed = speed;
+        this.buttSpace = buttSpace;
+        this.damage = damage;
+        this.weight = weight;
+    }
+
 }
