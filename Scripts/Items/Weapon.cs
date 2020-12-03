447749fc8c17df9f733b4c1b1a5ac3df1f54e28c
Faramarz Hosseini <sfhosseini-mousa@myseneca.ca>
2020-12-03 13:39:05 -0500

    folder

--- Scripts/Items/Weapon.cs
+++ Scripts/Items/Weapon.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Weapon : Equipable
 {
 
-    private int damage;
-    private double crit;
+    private int damage{get; set;}
+    private double crit{get; set;}
 
     public Weapon(int damage, double crit, string itemName, double weight, double price, int hero) : base(itemName, weight, price, hero)
     {
