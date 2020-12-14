3c81ba8c502cee9d916c687c4e3d9f708be89562
Faramarz Hosseini <sfhosseini-mousa@myseneca.ca>
2020-12-14 07:35:04 -0500

    item

---
+++ Scripts/Items/ArmorDatabase.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Sands {
+    public class ArmorDatabase {
+
+        private List<Armor> armors = new List<Armor>();
+
+        //get database
+        public List<Armor> getArmorList() {
+            return armors;
+        }
+
+        //get Hero at position
+        public Armor getArmor(int position) {
+            return armors[position];
+        }
+
+        //delete
+        public List<Armor> deleteArmor(Armor armor) {
+            return armors = armors.Except(armors.Where(a => a. == enemy.EnemyName)).ToList();
+        }
+
+        //clear
+        public void clearEnemyList() {
+            enemies.Clear();
+        }
+
+    }
+}
