444cd4b6ae7699f6d42ad42540277dc42690dbc6
Robert Parker-Lak <rparker-lak@myseneca.ca>
2020-12-19 15:32:49 -0500

    Vehicle database (not done)

---
+++ Scripts/Vehicles/VehicleDB.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sands {
+    public class VehicleDB : MonoBehaviour {
+
+        public List<Vehicle> vehicles = new List<Vehicle>();
+
+        void Awake() {
+            BuildVehicleDatabase();
+        }
+
+        void BuildVehicleDatabase() 
+        {
+            vehicles = new List<Vehicle>() {
+                //  int damage, double critDamage, int maxHP, int currentHP
+                //string name, int health, int armour, double speed, int buttspace, int damage, double weight) : base(name, health, armour, speed, buttspace, damage, weight
+                   new WarriorVehicle( ),
+                   new MageVehicle(  ),
+                   new RangerVehicle( )
+            };
+        }
+        
+
+        //get database
+        public List<Vehicle> getVehicleList() {
+            return vehicles;
+        }
+
+        //get vehicle at position
+        public Vehicle getVehicle(int position) {
+          return vehicles[position];
+        }
+
+        //clear
+        public void clearVehicleList() {
+            vehicles.Clear();
+        }
+    }
+}
