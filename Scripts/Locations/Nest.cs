01ded147e359025874a7a14d5ce6335fb5e15923
Faramarz Hosseini <sfhosseini-mousa@myseneca.ca>
2020-12-19 16:02:05 -0500

    nest

--- Scripts/Locations/Nest.cs
+++ Scripts/Locations/Nest.cs
@@ -6,13 +6,12 @@ namespace Sands
 {
     public class Nest : Location
     {
-        private Enemy enemySet{get;}
+        private List<Enemy> enemySet{get;}
 
 
-        public Nest(string name, double lattitude, double longtitude, int enemyName, int damage) : base(name, lattitude, longtitude)
+        public Nest(string name, double lattitude, double longtitude) : base(name, lattitude, longtitude)
         {
-            enemySet = new Enemy(enemyName, damage);
-
+            enemySet = new List<Enemy>();
         }
 
 
