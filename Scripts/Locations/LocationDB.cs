fcd66941679f2ea763a00789607ac08535afd326
Drew <43252026+AOSTAPOVYCH@users.noreply.github.com>
2020-12-22 17:26:27 -0500

    locations
    locations

--- Scripts/Locations/LocationDB.cs
+++ Scripts/Locations/LocationDB.cs
@@ -7,7 +7,6 @@ namespace Sands {
 
         private static List<Location> locations = new List<Location>();
 
-
         void Awake() {
             BuildClassDatabase();
         }
@@ -16,15 +15,17 @@ namespace Sands {
         {
             locations = new List<Location>() {
                 //string locationName, double latitude, double longitude
-            new Location("Norwich", 100,100),
-             new Location("Wearoy", 100,100),
-              new Location("Norwich", 100,100),
-               new Location("Norwich", 100,100),
                 new Location("Norwich", 100,100),
-                 new Location("Norwich", 100,100),
-                  new Location("Norwich", 100,100)
+                new Location("Westray", 100,100),
+                new Location("Gillamoor", 100,100),
+                new Location("Tunstead", 100,100),
+                new Location("Veden", 100,100),
+                new Location("Fara", 100,100),
+                new Location("Kaiser", 100,100),
+                new Location("Braedon", 100,100),
+                new Location("Helm Crest", 100,100),
+                new Location("Dalhurst", 100,100)
 
-            
             };
         }
     
