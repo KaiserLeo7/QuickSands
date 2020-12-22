f117e1dfeba92f7b503557081864067323ac73b7
Faramarz Hosseini <sfhosseini-mousa@myseneca.ca>
2020-12-22 16:01:51 -0500

    vehicle price

--- Scripts/Vehicles/VehicleClassDB.cs
+++ Scripts/Vehicles/VehicleClassDB.cs
@@ -17,10 +17,10 @@ namespace Sands {
                 //  int maxHP, int currentHP, int speed, int partySize, int capacity, int price
                 
                 //NEXT TIME BALANCE THESE
-                   new Scout(1000, 1000, 100, 2, 500, ),
-                   new Trader(3000, 3000, 60, 3, 2500, ),
-                   new Nomad(5000, 5000, 80, 4, 1000, ),
-                   new Goliath(8000, 8000, 70, 5, 1500, )
+                   new Scout(1000, 1000, 100, 2, 500, 3000),
+                   new Trader(3000, 3000, 60, 3, 2500, 7000),
+                   new Nomad(5000, 5000, 80, 4, 1000, 15000),
+                   new Goliath(8000, 8000, 70, 5, 1500, 30000)
             };
         }
         
