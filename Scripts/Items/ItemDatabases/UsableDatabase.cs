e40e40d371b07aa90a965e553fa1f360014a31e2
Drew <43252026+AOSTAPOVYCH@users.noreply.github.com>
2021-01-03 12:19:02 -0500

    p
    p

--- Scripts/Items/ItemDatabases/UsableDatabase.cs
+++ Scripts/Items/ItemDatabases/UsableDatabase.cs
@@ -17,9 +17,10 @@ namespace Sands
         void BuildClassDatabase()
         {
             usables = new List<Usable>() {
-                //string itemName, double weight, double price, int effectAmount
-                new Usable("Health Potion", 2, 100.0, 100),
-                new Usable("Repair Kit", 2, 200.0, 500)
+                //int ID, string itemName, double weight, double price, int effectAmount
+                new Usable(1, "Health Potion", 2, 100.0, 100),
+                new Usable(2, "Repair Kit", 2, 200.0, 500)
+                
             };
         }
 
