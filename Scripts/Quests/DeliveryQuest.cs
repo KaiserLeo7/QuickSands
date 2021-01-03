7d1b5de02e25a83f0a01cf5611a95cb10bc594bf
Faramarz Hosseini <sfhosseini-mousa@myseneca.ca>
2021-01-03 12:30:21 -0500

    f

--- Scripts/Quests/DeliveryQuest.cs
+++ Scripts/Quests/DeliveryQuest.cs
@@ -11,20 +11,12 @@ namespace Sands
 
         private System.Random random = new System.Random();
 
-<<<<<<< HEAD
-        public DeliveryQuest(string questDescription, string questLocation, int questReward)
-        {
-            this.questReward = random.Next();
-          //  this.questDescription = "Deliver " + random (random tear) item + "to "
-          // + random.locationlist - currentlocation;
-=======
         private ItemDatabase itemDatabase = new ItemDatabase();
 
         public DeliveryQuest(string questDescription, string questLocation, int questReward)
         {
             this.questReward = random.Next();
           this.questDescription = "Deliver " + random.Next(random tear) item + "to " + random.locationlist - currentlocation;
->>>>>>> parent of 7db1937... wtf 2.0
 
           //run locat
 
