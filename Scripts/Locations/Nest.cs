016b5759e1a8fb0884328e6e0b143d9999f154df
Faramarz Hosseini <sfhosseini-mousa@myseneca.ca>
2021-01-05 12:28:56 -0500

    Update Nest.cs

--- Scripts/Locations/Nest.cs
+++ Scripts/Locations/Nest.cs
@@ -7,6 +7,7 @@ namespace Sands
     public class Nest : Location
     {
         private EnemyClassDB enemyClassDB = new EnemyClassDB();
+        private List<Enemy> enemies;
         private List<Enemy> wave1;
         private List<Enemy> wave2;
         private List<Enemy> wave3;
@@ -14,20 +15,39 @@ namespace Sands
 
         public Nest(int id, string name, double lattitude, double longtitude, int territory, int[] nearbyTowns, int multiplier) : base(id, name, lattitude, longtitude, territory, nearbyTowns)
         {
-            wave1 = new List<Enemy>(){
+            enemies = new List<Enemy>(){
                 enemyClassDB.getEnemy(0),
                 enemyClassDB.getEnemy(0),
-                enemyClassDB.getEnemy(1)
-            };
-            wave2 = new List<Enemy>(){
+                enemyClassDB.getEnemy(1),
                 enemyClassDB.getEnemy(0),
                 enemyClassDB.getEnemy(0),
                 enemyClassDB.getEnemy(0),
                 enemyClassDB.getEnemy(1),
-                enemyClassDB.getEnemy(1)
+                enemyClassDB.getEnemy(1),
+                enemyClassDB.getEnemy(2)
+            };
+            
+            foreach (Enemy enemy in enemies)
+            {
+                enemy.Damage *= multiplier;
+                enemy.MaxHealth *= multiplier;
+                enemy.CurrentHealth *= multiplier;
+            }
+
+            wave1 = new List<Enemy>(){
+                enemies[0],
+                enemies[1],
+                enemies[2]
+            };
+            wave2 = new List<Enemy>(){
+                enemies[3],
+                enemies[4],
+                enemies[5],
+                enemies[6],
+                enemies[7]
             };
              wave3 = new List<Enemy>(){
-                enemyClassDB.getEnemy(2)
+                enemies[8]
             };
         }
     }
