054076dde1bcd5243f1655a85b15883b8a27af19
Drew <43252026+AOSTAPOVYCH@users.noreply.github.com>
2021-01-07 14:04:20 -0500

    g

--- Scripts/Locations/NestDB.cs
+++ Scripts/Locations/NestDB.cs
@@ -18,8 +18,8 @@ namespace Sands {
                
                 new Nest(1, "Black Rock", 100, 100, 1, new int[]{2}),
                 new Nest(2, "Infested Ruins", 100,100, 2, new int[]{1, 5, 10}),               
-                new Nest(3, "Swarming Mines", 100,100, 3, new int[]{9, 7, 2}),
-               // new Nest(4, "Infestation's Heart", 100,100, 3, new int[]{9, 7, 2})
+                new Nest(3, "Swarming Mines", 100,100, 3, new int[]{9, 7, 2})
+            
 
             };
         }
