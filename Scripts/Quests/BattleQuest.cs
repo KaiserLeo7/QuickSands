d2127c4f8fc992d563e578f1426f2379b0177e41
Faramarz Hosseini <sfhosseini-mousa@myseneca.ca>
2021-01-07 19:20:55 -0500

    nest downcasting

--- Scripts/Quests/BattleQuest.cs
+++ Scripts/Quests/BattleQuest.cs
@@ -13,18 +13,17 @@ namespace Sands
         public BattleQuest()
         {
             this.questLocation = nestDB.getNest(random.Next(0, 3));
-            Nest nest = (Nest)this.questLocation;
             this.questDescription = "Defeat the " + questLocation.LocationName + " Nest";
 
             if (playerData.CurrentLocation.Territory != questLocation.Territory)
             {
                 /////////IF DOESN'T WORK REMOVE MULTIPLIER IT'S ABOUT DOWNCASTING A PARENT TO A CHILD LOCATION TO NEST
-                this.questReward = random.Next(400*nest.Multiplier, 501*nest.Multiplier);
+                this.questReward = random.Next(400*((Nest)this.questLocation).Multiplier, 501*((Nest)this.questLocation).Multiplier);
                 this.distanceNote = "Far away";
             }
             else
             {
-                this.questReward = random.Next(300*nest.Multiplier, 401*nest.Multiplier);
+                this.questReward = random.Next(300*((Nest)this.questLocation).Multiplier, 401*((Nest)this.questLocation).Multiplier);
                 this.distanceNote = "Nearby";
             }
         }
