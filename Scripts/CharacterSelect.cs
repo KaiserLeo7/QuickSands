509179cee3026fb8511e579296929c2a3e717ac3
Robert Parker-Lak <rparker-lak@myseneca.ca>
2021-01-15 12:04:18 -0500

    charselect

---
+++ Scripts/CharacterSelect.cs
@@ -0,0 +1,30 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sands
+//party database and hero database, shoves into party database
+{
+    public class CharacterSelect
+    {
+        private HeroClassDB hero;
+        private HeroPartyDB party;
+
+        
+
+    }
+
+    public class addRanger(){
+       Hero ranger = hero.getHero(1);
+       party.add(ranger);
+    }
+
+    public class addWarrior(){
+
+    }
+
+    public class addMage(){
+
+    }
+
+}*/
